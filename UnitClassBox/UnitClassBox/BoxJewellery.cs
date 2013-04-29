df2029606e3673b3811bae5a201e2feea4abdf09
Andrey <andreyka4623@gmail.com>
2013-04-29 15:01:16 +0700

    First commit

---
+++ UnitClassBox/UnitClassBox/BoxJewellery.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelleryClass
+{
+    public class Jewellery
+    {
+        double _Weight;
+        public double Weight
+        {
+            get
+            {
+                return _Weight;
+            }
+        }
+
+        double _Price;
+        public double Price
+        {
+            get
+            {
+                return _Price;
+            }
+        }
+
+        string _Model;
+        public string Model
+        {
+            get
+            {
+                return _Model;
+            }
+        }
+
+
+        public Jewellery(double weight, double price, string model)
+        {
+            if (weight <= 0)
+                throw new ArgumentOutOfRangeException("weight");
+            if (price <= 0)
+                throw new ArgumentOutOfRangeException("price");
+            if (model == "")
+                throw new ArgumentNullException("model");
+            this._Model = model ;
+            this._Price = price;
+            this._Weight = weight;
+        }
+    }
+
+    public class Purchases
+    {
+       Jewellery[] Rings;
+
+       public Jewellery[] Golden_rings
+        {
+            get
+            {
+                return Rings;
+            }
+        }
+
+        public int Count()
+        {
+            return this.Rings.Count();
+        }
+
+        public Purchases(params Jewellery[] rings)
+        {
+            if (rings.Count() == 0)
+                throw new ArgumentNullException("rings");
+            this.Rings = rings;
+        }
+
+
+    }
+
+    public class PurchasesCounter
+    {
+        Purchases JewelleryBox;
+
+        public PurchasesCounter(Purchases box)
+        {
+            this.JewelleryBox = box;
+        }
+
+        public double GetTotalPrice()
+        {
+            double Price = 0;
+            for (int i = 0; i < JewelleryBox.Count(); i++)
+                Price += JewelleryBox.Golden_rings[i].Price * JewelleryBox.Golden_rings[i].Weight;
+            return Price;
+        }
+
+        public double GetTotalWeight()
+        {
+            double Weight = 0;
+            for (int i = 0; i < JewelleryBox.Count(); i++)
+                Weight += JewelleryBox.Golden_rings[i].Weight;
+            return Weight;
+        }
+    }
+}
+
+
---
+++ UnitTestBox/UnitTestBox/TestJewellery.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JewelleryClass;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class JewelleryBoxTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Jewellery_WhenWeightIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double WrongWeight = -3;
+            double CorrectPrice = 2000;
+            string JewelleryModel = "Golden ring";
+
+            //act
+            Jewellery golden = new Jewellery(WrongWeight, CorrectPrice, JewelleryModel);
+            // assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Jewellery_WhenPriceIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double CorrectWeight = 15;
+            double WrongPrice = -30;
+            string JewelleryModel = "Golden ring";
+
+            //act
+            Jewellery golden = new Jewellery(CorrectWeight, WrongPrice, JewelleryModel);
+            // assert is handled by ExpectedException
+        }
+
+       
+        [TestMethod]
+        public void Purchases_Count()
+        {
+            // arrange
+            Purchases box = new Purchases(new Jewellery(15, 2000, "Golden ring"),
+                new Jewellery(23, 3000, "Golden necklace"),
+                new Jewellery(15, 5500, "Platinum earrings"));
+             
+            int expected = 3;
+            //act
+            int actual = box.Count();
+
+            // assert
+            Assert.AreEqual(expected, actual, "Count has not been calculated correctly");
+        }
+
+
+        [TestMethod]
+        public void PurchasesCounter_Price()
+        {
+            // arrange
+            Purchases box = new Purchases(new Jewellery(10, 2000, "Golden ring"),
+                new Jewellery(20, 6000, "Golden necklace"),
+                new Jewellery(15, 5500, "Platinum earrings"));
+            PurchasesCounter counter = new PurchasesCounter(box);
+            double expected = 222500;
+            //act
+            double actual = counter.GetTotalPrice();
+
+            // assert
+            Assert.AreEqual(expected, actual, 0.001, "TotalPrice has not been calculated correctly");
+        }
+
+        [TestMethod]
+        public void GiftCalculator_Weight()
+        {
+            // arrange
+            Purchases box = new Purchases(new Jewellery(10, 2000, "Golden ring"),
+                new Jewellery(20, 6000, "Golden necklace"),
+                new Jewellery(15, 5500, "Platinum earrings"));
+            PurchasesCounter counter = new PurchasesCounter(box);
+            double expected = 45;
+            //act
+            double actual = counter.GetTotalWeight();
+
+            // assert
+            Assert.AreEqual(expected, actual, 0.001, "TotalWeight has not been not calculated correctly");
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Jewellery_WhenJewelleryModelNamelHasName_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double CorrectWeight = -3;
+            double CorrectPrice = 2000;
+            string JewelleryModel = "";
+
+            //act
+            Jewellery golden = new Jewellery(CorrectWeight, CorrectPrice, JewelleryModel);
+            // assert is handled by ExpectedException
+                
+            
+        }
+    
+    
+    }
+}
+
